29192e345b17f6ffe7e2916dcf625d4b3ccc563b
yahav3801 <ybh00790@gmail.com>
2025-06-22 18:23:24 +0300

    sidebar setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSubItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <Sidebar collapsible="icon" className="overflow-hidden">
+        <SidebarHeader className="flex-row">
+          <SidebarTrigger />
+          <span className="text-lg text-nowrap">YBH Jobs</span>
+        </SidebarHeader>
+        <SidebarContent>asdasd</SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>asdasd</SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+    </SidebarProvider>
+  );
 };
 
 export default page;
